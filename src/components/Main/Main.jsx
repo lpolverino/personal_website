fdf0a42c4c6106d3cb3e06614e18225dad960317
lpolverino <lorenz.polverino@gmail.com>
2024-01-11 19:55:57 -0300

    fixed section heigh and added contact section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Home from '../Home/Home'
 import About from '../About/About'
 import Projects from '../Projects/Projects'
+import Contact from '../Contact/Contact'
+import style from "./main.module.css"
 
 
 const Main = () => {
   return (
-    <div style={{height:"100%"}}>
+    <div className={style.main}>
       <Home></Home>
       <About></About>
       <Projects></Projects>
+      <Contact></Contact>
     </div>
   )
 }
